2348db1da41d07de20f80be6d7adff7073d910ee
Mike <againstthesilence@gmail.com>
2024-10-06 21:20:56 +0300

    mplemented function that reads file fileToRead.txt content using Readable Stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+const { createHash } = await import("node:crypto");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __filePath = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const input = createReadStream(__filePath);
+  input.pipe(stdout);
 };
 
 await read();
