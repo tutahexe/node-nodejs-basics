5326fee7266f2b1969de98245ffe66b4f39a2f48
Mike <againstthesilence@gmail.com>
2024-10-06 15:03:41 +0300

    implemented function that copies folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import * as fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const content = "I am fresh and young";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __sourceDir = path.join(__dirname, "files");
+const __copyDir = path.join(__dirname, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  fs.access(__copyDir, (err) => {
+    if (!err) {
+      console.error("FS operation failed");
+    } else {
+      fs.cp(__sourceDir, __copyDir, { recursive: true }, (err) => {
+        if (err) {
+          console.error("FS operation failed");
+        } else {
+          console.log("files copied");
+        }
+      });
+    }
+  });
 };
 
 await copy();
