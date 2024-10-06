ea7c928b0090dd4d96d4acc792a449870d6b4876
Mike <againstthesilence@gmail.com>
2024-10-06 16:36:47 +0300

    implemented function that prints content of the fileToRead.txt

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import * as fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __file = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  fs.access(__file, (err) => {
+    if (err) {
+      console.error("FS operation failed");
+    } else {
+      fs.readFile(__file, "utf8", (err, data) => {
+        if (err) {
+          console.error("FS operation failed");
+        } else {
+          console.log(data);
+        }
+      });
+    }
+  });
 };
 
 await read();
