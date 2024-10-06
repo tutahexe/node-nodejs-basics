51b784dddeae9fe66624b9bb2ed01a6b1c5fa4c4
Mike <againstthesilence@gmail.com>
2024-10-06 16:37:30 +0300

    small naming fixes

--- src/fs/create.js
+++ src/fs/create.js
@@ -5,10 +5,10 @@ import { fileURLToPath } from "url";
 const content = "I am fresh and young";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const filePath = path.join(__dirname, "files", "fresh.txt");
+const __filePath = path.join(__dirname, "files", "fresh.txt");
 
 const create = async () => {
-  fs.writeFile(filePath, content, (err) => {
+  fs.writeFile(__filePath, content, (err) => {
     if (err) {
       console.error("FS operation failed");
     } else {
