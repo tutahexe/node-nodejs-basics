f4b8786ea31e248b0969c4e1f95db6f8e294f77e
Mike <againstthesilence@gmail.com>
2024-10-07 11:04:09 +0300

    implemented function that reads data from process.stdin, reverses text

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,7 +2,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { createReadStream } from "node:fs";
 import { stdout } from "node:process";
-const { createHash } = await import("node:crypto");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
